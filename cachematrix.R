6468267f8b9368af44075f4d7b6bcc9ddf2b1d7d
William Kay <williamkay04@gmail.com>
2020-05-05 15:37:53 -0500

    Solution for programming assignment 2 in R programing course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## Creates two functions, makeCacheMatrix and cacheSolve which returns the
+## inverse of a matrix after first checking for a cached value from an earlier
+## computation
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## makeCacheMatrix returns a list of several functions manipulating a given 
+## matrix, x. 
+
+makeCacheMatrix <- function(x= matrix()){
+        inv <- NULL #Varriable inv initalized to NULL
+        get <-function() x #get() simply returns the given matrix
+        setinv <- function(i) inv <<- i # serinv takes a given value, i
+        #and sets the inverse of the matrix to that value
+        getinv<- function() inv #getinv returns the inverse of the matrix 
+        
+        list(set = set, get=get, setinv=setinv, getinv=getinv) #makeCahceMatrix returns a list of the above functions
+       }
+
+
+## cacheSolve taxes an input in the form of the list returned from makeCahceMatrix
+## and searches for and returns a cached inverse of the matrix. If no cahce is avaliable it
+## computes the inverse and stores it within the cahce
+
+cacheSolve <- function(x, ...)  {
+        z<-x$getinv() #z is set to the invers of the input matrix 
+        if(!is.null(z)){
+               print("Getting cached data") #if z is not null (ie has a cached value, then z is returned)
+               return(z)
+       }
+       data<-x$get() #data is retrived from the input matrix
+       i<<- solve(data, ...) #i is set to the inverse of the matrix using solve function
+       x$setinv(i) #i is then stored in the cache of the object x
+       i   ## Return a matrix that is the inverse of 'x'
 }
